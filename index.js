526c789806e160c1e5a50493f2240a65c06152e4
Ali G <ali.glenesk@gmail.com>
2018-03-10 11:03:44 -0800

    adds images

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ import Header from './src/components/Header'
 import AlbumList from './src/components/AlbumList'
 
 const App = () => (
-  <View>
+  <View style={{ flex: 1 }}>
     <Header headerText={'albums'} />
     <AlbumList />
   </View>
   )
-
 
 // reminder, fat arrow function has implicit return
 AppRegistry.registerComponent('albums', () => App)
