199b8a71543c97c857e8092d14bb118722643337
Ali G <ali.glenesk@gmail.com>
2018-03-10 12:07:42 -0800

    first app complete

--- index.js
+++ index.js
@@ -13,3 +13,6 @@ const App = () => (
 // reminder, the root component for the app must have flex of 1 to enable scrolling
 // reminder, fat arrow function has implicit return
 AppRegistry.registerComponent('albums', () => App)
+
+// rewatch app wrapup (lecture 54) for review
+
