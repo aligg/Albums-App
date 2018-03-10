df10bf87c9952cc826aa5d8c42bf83c9a4163183
Ali G <ali.glenesk@gmail.com>
2018-03-10 09:30:16 -0800

    Adds API call

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import axios from 'axios'
 
 class AlbumList extends Component {
+
+  state = { albums: [] }
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => this.setState({ albums: response.data }))
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>)
+  }
+
   render() {
     return (
       <View>
-        <Text>Album List </Text>
+        {this.renderAlbums()}
       </View>
     )
   }
@@ -38,4 +51,16 @@ eg:
 
 class ComponentName extends Component 
 class base components need render method!
+
+Life Cycle methods: 
+ 
+ functions that we place on our class that wil be automatically called at some point.
+
+ componentWillMount is a lifecycle method
+
+
+ state vs props!
+
+ from parent to child component = props
+ for component's internal record keeping =  state 
 */
